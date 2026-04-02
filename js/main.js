986eb8761f568b05c6e26ccc61343fe729afc97a
fuseann28 <fuseann28@gmail.com>
2026-04-02 11:08:52 -0500

    alarm clock and tamp and positioning

--- js/main.js
+++ js/main.js
@@ -4,7 +4,24 @@ document.querySelector('.bow').addEventListener('click', function() {
     this.classList.toggle('rotate')
     });
 
+document.querySelector('.lamp-string').addEventListener('click', () => {
+  document.querySelector('.lamp').classList.toggle('on');
+});
 
+let alarmStarted = false;
 
+window.addEventListener("click", () => {
+    console.log('started');
+  if (alarmStarted) return;   // prevents multiple timers
+  alarmStarted = true;
+
+  setTimeout(() => {
+    const alarm = document.getElementById("alarmsound");
+    const clock = document.querySelector(".clock");
+
+    alarm.play();
+    clock.classList.add("ringing");
+  }, 3000); // 10 seconds
+});
 
 
